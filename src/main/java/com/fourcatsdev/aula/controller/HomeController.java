1bbd1b0b232ebc2db047aacf8fa96bdea9e94e93
Adroaldo <hygino@msn.com>
2022-07-23 15:43:27 -0300

    Home controller criado

---
+++ src/main/java/com/fourcatsdev/aula/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.fourcatsdev.aula.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("msnBemVindo", "Bem vindo à biblioteca");
+        return "publica-index";
+    }
+}
